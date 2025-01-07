251f9d0a9c22ff3e008042ca853a4dcbcb3c3b57
Wesley Henrique Silva da Silva <182400738@senacinformatica.br>
2025-01-07 17:21:54 -0300

    adição para o futuro

--- campoMinado2.py
+++ campoMinado2.py
@@ -2,11 +2,20 @@ import random
 import tkinter as tk
 
 class CampoMinado:
-    def __init__(self, root):
+    def __init__(self,root,tamanho,num_minas):
+        self.tamanho = tamanho
+        self.num_minas = num_minas
+        self.tabuleiro = []
+        self.revelado = []
+        self.botoes = []
+        self.minas = set()
+        self.jogo_terminado = False
         self.root = root
-        self.root.geometry("800x600")
-        self.root.configure(bg="red")
+        self.root.geometry("900x700")
+        self.root.configure(bg="grey")
         self.root.title("Campo Minado")
+        
+        
         
 
 
@@ -15,5 +24,11 @@ class CampoMinado:
 
 
 
-campo_minado = CampoMinado(tk.Tk())
-campo_minado.root.mainloop()
+def inicio_do_jogo():
+    root = tk.Tk()
+    tamanho = 9
+    num_minas = 10
+    CampoMinado(root,tamanho,num_minas)
+    root.mainloop()
+
+inicio_do_jogo()
